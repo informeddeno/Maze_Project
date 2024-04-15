66f577b8087de028f4b839c55680ca08479b031b
informeddeno <informeddeno@gmail.com>
2024-04-15 08:07:57 -0700

    Maze_Project

--- src/main.c
+++ src/main.c
@@ -95,6 +95,11 @@ int main(void)
 
 	
 	SDL_Window *window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	bool initializeWindow(void);
+	 if (!initializeWindow()) {
+        fprintf(stderr, "Error initializing window.\n");
+        return 1; // Exit with error
+    }
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
