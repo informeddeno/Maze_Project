f445d48149e11eb489045d1c6ad0d6c54e8c5aac
informeddeno <informeddeno@gmail.com>
2024-04-15 06:17:58 -0700

    Maze_Project

--- src/window.c
+++ src/window.c
@@ -1,18 +1,48 @@
 #include "../headers/header.h"
 
-static SDL_Window		*window = NULL;
-static SDL_Renderer		*renderer = NULL;
-static uint32_t			*colorBuffer = NULL;
-static SDL_Texture		*colorBufferTexture;
+static SDL_Renderer *renderer;
+static color_t *colorBuffer;
+static SDL_Texture *colorBufferTexture;
+static SDL_Window *window;
 
-bool	initializeWindow(void)
+/**
+ * initializeWindow - Initialize window to display the maze
+ * Return: true in case of success, false if it fails
+*/
+bool initializeWindow(void)
 {
+  SDL_DisplayMode display_mode;
+	int fullScreenWidth, fullScreenHeight;
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		fprintf(stderr, "Error initializing SDL.\n");
 		return (false);
 	}
-	window = SDL_CreateWindow
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+	fullScreenWidth = display_mode.w;
+	fullScreenHeight = display_mode.h;
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		fullScreenWidth,
+		fullScreenHeight,
+		SDL_WINDOW_BORDERLESS
+		);
+  /**  SDL_DisplayMode display_mode;
+  int WINDOW_WIDTH; int WINDOW_HEIGHT;  
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		fprintf(stderr, "Error initializing SDL.\n");
+		return (false);
+	}
+  WINDOW_WIDTH = display_mode.w;
+  WINDOW_HEIGHT = display_mode.h;
+
+
+  window = SDL_CreateWindow
 	(
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
@@ -21,12 +51,14 @@ bool	initializeWindow(void)
 		WINDOW_HEIGHT,
 		SDL_WINDOW_RESIZABLE
 	);
+**/
+	
 	if (!window)
 	{
 		fprintf(stderr, "Error creating SDL window.\n");
 		return (false);
 	}
-	renderer = SDL_CreateRenderer(window, -1, 0);
+	renderer = SDL_CreateRenderer(window, -1, 1);
 	if (!renderer)
 	{
 		fprintf(stderr, "Error creating SDL renderer.\n");
@@ -34,23 +66,22 @@ bool	initializeWindow(void)
 	}
 	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
-	// allocate the total amount of bytes in memory to hold our colorbuffer
-	colorBuffer = (uint32_t *)malloc(sizeof(uint32_t) * (uint32_t)WINDOW_WIDTH * (uint32_t)WINDOW_HEIGHT);
+	/* allocate the total amount of bytes in memory to hold our colorbuffer */
+	colorBuffer = (color_t*)malloc(sizeof(color_t) * SCREEN_WIDTH * SCREEN_HEIGHT);
 
-	// create an SDL_texture to display the colorbuffer
-	colorBufferTexture = SDL_CreateTexture
-	(
-		renderer,
-		SDL_PIXELFORMAT_RGBA32,
-		SDL_TEXTUREACCESS_STREAMING,
-		WINDOW_WIDTH,
-		WINDOW_HEIGHT
-	);
+	/* create an SDL_Texture to display the colorbuffer */
+	colorBufferTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32,
+		SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	return (true);
 }
 
-void	destroyWindow(void)
+/**
+ * destroyWindow - destroy window when the game is over
+ *
+*/
+
+void destroyWindow(void)
 {
 	free(colorBuffer);
 	SDL_DestroyTexture(colorBufferTexture);
@@ -59,74 +90,44 @@ void	destroyWindow(void)
 	SDL_Quit();
 }
 
-void	clearColorBuffer(color_t color)
+
+/**
+ * clearColorBuffer - clear buffer for every frame
+ * @color: color buffer
+*/
+
+void clearColorBuffer(color_t color)
 {
-	for (int i = 0; i < WINDOW_WIDTH * WINDOW_HEIGHT; i++)
+		
+	for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) 
 		colorBuffer[i] = color;
 }
 
-void	renderColorBuffer(void)
+/**
+ * renderColorBuffer - render buffer for every frame
+ *
+*/
+
+void renderColorBuffer(void)
 {
-	SDL_UpdateTexture
-	(
+	SDL_UpdateTexture(
 		colorBufferTexture,
 		NULL,
 		colorBuffer,
-		(int)((uint32_t)WINDOW_WIDTH * sizeof(color_t))
+		(int)(SCREEN_WIDTH * sizeof(color_t))
 	);
 	SDL_RenderCopy(renderer, colorBufferTexture, NULL, NULL);
 	SDL_RenderPresent(renderer);
 }
 
-void	drawPixel(int x, int y, color_t color)
+/**
+ * drawPixel - assign a color to each pixel
+ * @x: x pixel coordinate
+ * @y: y pixel coordinate
+ * @color: pixel color
+*/
+
+void drawPixel(int x, int y, color_t color)
 {
-	colorBuffer[(WINDOW_WIDTH * y) + x] = color;
-}
-
-void	drawRect(int x, int y, int width, int height, color_t color)
-{
-	for (int i = x; i <= (x + width); i++)
-	{
-		for (int j = y; j < (y + height); j++)
-		{
-			drawPixel(i, j, color);
-		}
-	}
-}
-
-void	drawLine(int x0, int y0, int x1, int y1, color_t color)
-{
-	int		deltaX;
-	int		deltaY;
-	int		longestSideLength;
-	float	xIncrement;
-	float	yIncrement;
-	float	currentX;
-	float	currentY;
-
-	// diferences between start and end of the line
-	deltaX = (x1 - x0);
-	deltaY = (y1 - y0);
-
-	// longest side of the line
-	longestSideLength = (abs(deltaX) >= abs(deltaY)) ? abs(deltaX) : abs(deltaY);
-
-	// find the increment values
-	xIncrement = deltaX / (float)longestSideLength;
-	yIncrement = deltaY / (float)longestSideLength;
-
-	// start point
-	currentX = x0;
-	currentY = y0;
-
-	// loop all the longest side until the end
-	for (int i = 0; i < longestSideLength; i++)
-	{
-		// draw pixel, rounding the values to integer to get nearest pixel
-		drawPixel(round(currentX), round(currentY), color);
-
-		// increment the slope to get the next pixel
-		currentX += xIncrement;
-		currentY += yIncrement;
-	}
+	colorBuffer[(SCREEN_WIDTH * y) + x] = color;
 }
