c69d7edced2f07fd6c296e48fac70eea0a0855f8
informeddeno <informeddeno@gmail.com>
2024-04-15 07:29:59 -0700

    Maze_Project

--- src/main.c
+++ src/main.c
@@ -93,6 +93,8 @@ int main(void)
         printf("SDL video system is ready to go\n");
 	}
 
+	SDL_createWindow();
+
 	SDL_Window *window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
